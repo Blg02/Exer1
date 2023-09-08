feec89faa4f0dba913d23612ddfcdb71855b05fc
Blg02 <88237092+Blg02@users.noreply.github.com>
2023-09-08 10:22:00 +0800

    Update javascript.js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -6,15 +6,15 @@ function displaySum() {
   document.getElementById("answer").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
 }
 
-document.getElementById('sumButton').addEventListener("click", displaySum);
-
 function displayDiff() {
   let firstNum = Number(document.getElementById('firstNum').innerHTML)
   let secondNum = Number(document.getElementById('secondNum').innerHTML)
 
-  let total1 = firstNum - secondNum;
-  document.getElementById("answer").innerHTML = ` ${firstNum} - ${secondNum}, equals to ${total1}` ;
+  let total = firstNum - secondNum;
+  document.getElementById("answer").innerHTML = ` ${firstNum} - ${secondNum}, equals to ${total}` ;
 }
 
+
+document.getElementById('sumButton').addEventListener("click", displaySum);
 document.getElementById('diffButton').addEventListener("click", displayDiff);
 
