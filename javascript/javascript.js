202d51f31c4e6da2d11e621213e7131459f9b285
Blg02 <88237092+Blg02@users.noreply.github.com>
2023-09-08 10:12:44 +0800

    Update javascript.js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -13,7 +13,7 @@ function displayDiff() {
   let secondNum = Number(document.getElementById('secondNum').innerHTML)
 
   let total = firstNum - secondNum;
-  document.getElementById("answer").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
+  document.getElementById("answer").innerHTML = ` ${firstNum} - ${secondNum}, equals to ${total}` ;
 }
 
 document.getElementById('diffButton').addEventListener("click", displayDiff);
