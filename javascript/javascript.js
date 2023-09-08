6a6eb022640c8b5abca7b94a47f7f697add1aa66
Blg02 <88237092+Blg02@users.noreply.github.com>
2023-09-08 11:05:26 +0800

    Update javascript.js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -14,7 +14,25 @@ function displayDiff() {
   document.getElementById("diffanswer").innerHTML = ` ${firstNum} - ${secondNum}, equals to ${total}` ;
 }
 
+function displayProd() {
+  let firstNum = Number(document.getElementById('firstNum').innerHTML)
+  let secondNum = Number(document.getElementById('secondNum').innerHTML)
+
+  let total = firstNum * secondNum;
+  document.getElementById("prodanswer").innerHTML = ` ${firstNum} * ${secondNum}, equals to ${total}` ;
+}
+
+function displayQuo() {
+  let firstNum = Number(document.getElementById('firstNum').innerHTML)
+  let secondNum = Number(document.getElementById('secondNum').innerHTML)
+
+  let total = firstNum / secondNum;
+  document.getElementById("quoanswer").innerHTML = ` ${firstNum} / ${secondNum}, equals to ${total}` ;
+}
+
 
 document.getElementById('sumButton').addEventListener("click", displaySum);
 document.getElementById('diffButton').addEventListener("click", displayDiff);
+document.getElementById('prodButton').addEventListener("click", displayProd);
+document.getElementById('quoButton').addEventListener("click", displayQuo);
 
